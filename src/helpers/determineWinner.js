fb740ed7d14fe1bd668f0c1d05172d304a046327
Erik Björn <erik.g.bjoern@gmail.com>
2020-07-25 18:13:59 +0200

    refactoring determineWinner module

--- src/helpers/determineWinner.js
+++ src/helpers/determineWinner.js
@@ -1,23 +1,18 @@
-export const determineWinner = (leftChoice, rightChoice) => {
-  
-  const winner = compare(leftChoice, rightChoice)
-  return winner
-}
-  
-  const compare = (leftChoice, rightChoice) => {
-    if (leftChoice === rightChoice) {
+const compare = (leftChoice, rightChoice) => {
+  switch(leftChoice) {
+    case rightChoice:
       return 'Tie'
-    } else if ((leftChoice === "rock")     && (rightChoice === "scissors")) {
-      return 'Left player wins'
-    } else if ((leftChoice === "rock")     && (rightChoice === "paper")) {
-      return 'Right player wins'
-    } else if ((leftChoice === "paper")    && (rightChoice === "rock")) {
-      return 'Left player wins'
-    } else if ((leftChoice === "paper")    && (rightChoice === "scissors")) {
-      return 'Right player wins'
-    } else if ((leftChoice === "scissors") && (rightChoice === "paper")) {
-      return 'Left player wins'
-    } else if ((leftChoice === "scissors") && (rightChoice === "rock")) {
-      return 'Right player wins'
-    }
+    case "rock":
+      return rightChoice === "scissors" ? 'Left player wins' : 'Right player wins'
+    case "paper":
+      return rightChoice === "rock" ? 'Left player wins' : 'Right player wins'
+    case "scissors":
+      return rightChoice === "paper" ? 'Left player wins' : 'Right player wins'
+    default: 
+      break
   }
+}
+
+export const determineWinner = (leftChoice, rightChoice) => (
+  compare(leftChoice, rightChoice)
+)
