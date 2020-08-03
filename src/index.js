378509cebf832b59450b3427ec53e1b2d4017067
Erik Björn <erik.g.bjoern@gmail.com>
2020-08-03 13:18:06 +0200

    removing semantic import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import './App.css'
 import './fonts/Bangers-Regular.ttf'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-        <App />, document.getElementById("root")
+  <App />, document.getElementById("root")
 )
 
 serviceWorker.register();
