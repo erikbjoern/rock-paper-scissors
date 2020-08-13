38f01f12fa8d4062b2c47d405eec23a5baaaddd7
Erik Björn <erik.g.bjoern@gmail.com>
2020-08-13 20:42:47 +0200

    bringing back a straight line fuse

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -6,46 +6,27 @@ const Players = ({ countdown, rightChoice, leftChoice }) => {
   const tooSlow = countdown <= 0 && countdown >= -2;
 
   const makeAChoice = (
-    <>
-      {countingDown && <p className="make-a-choice">make a choice!</p>}
+    <div className="makeAChoiceContainer">
+      {countingDown && (
+        <>
+          <p className="make-a-choice">make a choice!</p>
+          <div className="fuse" />
+        </>
+      )}
       {tooSlow && <p className="slow">too slow!</p>}
-    </>
+    </div>
   );
 
   return (
     <div className="playerContainer">
       <div className="leftPlayer">
-        <Hand
-          side="Left"
-          choice={leftChoice}
-          countdown={countdown}
-        />
+        <Hand side="Left" choice={leftChoice} countdown={countdown} />
         {!leftChoice && makeAChoice}
       </div>
-      <svg className="lFuseContainer">
-        <circle
-          className={countingDown ? "fuse" : ""}
-          cx="175"
-          cy="175"
-          r="150"
-        />
-      </svg>
       <div className="rightPlayer">
-        <Hand
-          side="Right"
-          choice={rightChoice}
-          countdown={countdown}
-        />
+        <Hand side="Right" choice={rightChoice} countdown={countdown} />
         {!rightChoice && makeAChoice}
       </div>
-      <svg className="rFuseContainer">
-        <circle
-          className={countingDown ? "fuse" : ""}
-          cx="175"
-          cy="175"
-          r="150"
-        />
-      </svg>
     </div>
   );
 };
